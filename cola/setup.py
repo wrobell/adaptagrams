17de137ffc86ba6b8c11beba75484918160585d5
Arjan Molenaar <gaphor@gmail.com>
2011-03-11 17:06:49 +0100

    Added setup.py on toplevel. Builds libavoid itself.

--- cola/setup.py
+++ cola/setup.py
@@ -1,11 +1,11 @@
 
-#from distutils.core import setup
-from setuptools import setup #, find_packages
+from distutils.core import setup
+#from setuptools import setup #, find_packages
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 sources = [
-    'libavoid.pyx',
+    'python/libavoid.pyx',
     'libavoid/connectionpin.cpp',
     'libavoid/connector.cpp',
     'libavoid/connend.cpp',
@@ -22,33 +22,50 @@ sources = [
     'libavoid/vertices.cpp',
     'libavoid/viscluster.cpp',
     'libavoid/visibility.cpp',
-    'libavoid/vpsc.cpp',
-    ]
+    'libavoid/vpsc.cpp'
+]
+
+#examples = [
+#    'test_multiconnact',
+#    'test_example',
+#]
+#
+#example_extensions = [Extension(
+#    example,
+#    sources=[example + '.pyx'],
+#    language="c++",
+#    include_dirs=['.', '..'],
+#    #extra_link_args=[...],
+#    library_dirs=['../libavoid/.libs'],
+#    libraries=['avoid']
+#    ) for example in examples]
+
 
 setup(
-    name='pylibavoid',
+    name='libavoid',
     version='0.1',
 
     author="Arjan J. Molenaar",
     author_email='gaphor@gmail.com',
     
-    setup_requires = [
-     'nose >= 0.10.4',
-     'setuptools-git >= 0.3.4'
-    ],
-
-    test_suite = 'nose.collector',
+#    setup_requires = [
+#     'nose >= 0.10.4',
+#     'setuptools-git >= 0.3.4'
+#    ],
+#
+#    test_suite = 'nose.collector',
     
-    #cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension(
         "libavoid",
-        sources,
+        sources=sources,
         language="c++",
         include_dirs=['.'],
         #extra_link_args=[...],
-        )],
+        #library_dirs=['../libavoid/.libs'],
+        #libraries=['avoid']
+        )], # + example_extensions,
 
-    zip_safe=False,
+#    zip_safe=False,
 
     cmdclass = {'build_ext': build_ext}
 )
