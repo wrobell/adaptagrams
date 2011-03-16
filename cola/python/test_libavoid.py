9cbe706fe20408a42ab18e230c14a375ae63661b
Arjan Molenaar <gaphor@gmail.com>
2011-03-16 18:15:42 +0100

    merged test files.

--- cola/python/test_libavoid.py
+++ cola/python/test_libavoid.py
@@ -175,4 +175,44 @@ def test_routing_with_output():
     #del router
 
 
+#gc.set_debug(gc.DEBUG_STATS | gc.DEBUG_COLLECTABLE | gc.DEBUG_UNCOLLECTABLE | gc.DEBUG_INSTANCES)
+
+def callback(data):
+    data.append(True)
+
+#@nottest
+def test_connref_callback():
+
+    assert_equals(2, sys.getrefcount(callback))
+    router = Router()
+    shape = ShapeRef(router, rectangle((2, -2), (6, 2)))
+    router.addShape(shape)
+    conn = ConnRef(router, (0, 0), (20, 0))
+    outlist = []
+#    assert_equals(2, sys.getrefcount(conn))
+
+    # TODO: Here we can introduce circular references
+    conn.setCallback(callback, outlist)
+    router.processTransaction()
+    assert_equals(1, len(outlist))
+    assert_equals(3, sys.getrefcount(callback))
+    router.moveShapeRel(shape, 1, 0)
+    router.processTransaction()
+    assert_equals(2, len(outlist))
+    
+    conn.setCallback(None, None)
+    router.moveShapeRel(shape, -1, 0)
+    router.processTransaction()
+    assert_equals(2, len(outlist))
+    assert_equals(2, sys.getrefcount(callback))
+
+#    del router# , shape, conn, outlist
+#    for i in range(10):
+#        gc.collect()
+#    print '*** finished collect'
+#    del conn, shape
+#    gc.collect()
+#
+#    del router
+
 # vim:sw=4:et:ai
